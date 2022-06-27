19178661970a9328422387cd51c9bbe975261330
marc <marcusher@outlook.com>
2022-06-27 11:52:10 +0100

    Added authentication & authorisation functionality for signup, login, logout and password change for a logged in user

--- cakeproject/main_app/forms.py
+++ cakeproject/main_app/forms.py
@@ -1,1 +1,18 @@
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm, PasswordResetForm
+from django.contrib.auth.models import User
+
+
+# Custom Form for Sign-up - extending the UserCreationForm to include email address as a field:
+
+class NewUserForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ['username', 'email', 'password1', 'password2']
+  
+  def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+          user.save()
+        return user
--- cakeproject/main_app/urls.py
+++ cakeproject/main_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+
+    # Authentication Paths
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/password_change/', views.PasswordChange.as_view(), name='password_change'),
 ]
